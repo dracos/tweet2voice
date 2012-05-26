e630b354e6b3bf53b942ab5521c09b7fb67cc3b3
Matthew Somerville <matthew-github@dracos.co.uk>
2012-05-26 16:43:55 +0100

    Put back OAuth option in order that this can be used on protected accounts.

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ on Ubuntu, so currently only works on them. Patches to work on more systems
 welcome!
 
 Copy config.py-example to config.py, and edit to have the names of the people
-of interest, and your Twitter login details.
+of interest, and your Twitter login details (or OAuth parameters if you want
+this to work on protected people; you'll need a registered Twitter app).
 
 Install tweepy (using pip install -r requirements.txt will do that, preferably
 in a virtualenv), then you can just run:
 
 $ python -u tweet2voice.py
 
-I'm using a fork of tweepy that fixes a streaming bug in it.
+I'm using my own fork of a fork of tweepy that fixes a streaming bug in it, and
+adds some functionality to the user stream call.
--- tweet2voice.py
+++ tweet2voice.py
@@ -4,11 +4,12 @@ import subprocess
 import tweepy
 from config import *
 
-auth = tweepy.BasicAuthHandler(USERNAME, PASSWORD)
 api = tweepy.API()
 
 class Listener ( tweepy.StreamListener ):
   def on_status( self, status ):
+    if status.author.id not in user_ids:
+        return True
     print status.author.screen_name, status.text
     try:
         subprocess.call( [ 'say', status.text ] )
@@ -19,9 +20,28 @@ class Listener ( tweepy.StreamListener ):
             pass
     return True
 
+# We need the IDs of the given Twitter user names
+# We can also then check if any are protected accounts
+users = api.lookup_users( screen_names=PEOPLE_OF_INTEREST )
+user_ids = [ user.id for user in users ]
+users_protected = [ user for user in users if user.protected ]
+if users_protected:
+    if not CONSUMER_KEY:
+        raise Exception, "You need to use OAuth if you want this to work on a protected account"
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+else:
+    auth = tweepy.BasicAuthHandler(USERNAME, PASSWORD)
+
 listener = Listener()
 stream = tweepy.Stream( auth=auth, listener=listener )
 
-user_ids = [ x.id for x in api.lookup_users( screen_names=PEOPLE_OF_INTEREST ) ]
-stream.filter( follow=user_ids )
+if users_protected:
+    # We want to additionally follow any IDs for non-private people,
+    # as well as the user stream
+    user_ids_public = [ user.id for user in users if not user.protected ]
+    stream.userstream( follow=user_ids_public )
+else:
+    # We want to follow the IDs of everyone
+    stream.filter( follow=user_ids )
 
